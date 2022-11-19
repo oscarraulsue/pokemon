31fdde407cd83a48075bc1ed3cdc8d274dddfff3
Oscar Sue Alvarado <oscar.sue@confeorg.loc>
2022-11-19 18:10:00 -0500

    [ADD] ISR

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -118,6 +118,22 @@ const PokemonPage: NextPage<Props> = ({ pokemon }) => {
 
 // You should use getStaticPaths if you’re statically pre-rendering pages that use dynamic routes
 
+
+export const getStaticPaths: GetStaticPaths = async (ctx) => {
+
+  const pokemons151 = [...Array(151)].map( ( value, index ) => `${ index + 1 }` );
+
+  return {
+    paths: pokemons151.map( id => ({
+      params: { id }
+    })),
+    // fallback: false
+    fallback: 'blocking'
+  }
+}
+
+
+
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   
   const { id } = params as { id: string };
@@ -133,14 +149,12 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     }
   }
 
-
   return {
     props: {
       pokemon
     },
   }
 }
-
 
 
 export default PokemonPage;
